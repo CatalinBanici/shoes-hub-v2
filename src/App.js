683d4cc0a9da05559be40285789850e5135c6161
Catalin Banici <catalinbanici95@yahoo.com>
2024-01-09 18:35:29 +0200

    first routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RootPage from "./pages/root/RootPage";
+import HomePage from "./pages/home/HomePage";
+import StorePage from "./pages/store/StorePage";
+
 function App() {
-  return <div className="App"></div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootPage />}>
+        <Route index element={<HomePage />} />
+        <Route path="store" element={<StorePage />} />
+      </Route>,
+    ),
+  );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
